c3e3a197287172bcd8b8d9f23b9c0694b05ab2ab
AG <ag.nayanajith@gmail.com>
2025-04-30 22:58:31 +0530

    Init

---
+++ Api/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddNwsManager();
+
+builder.AddServiceDefaults();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+// Map the endpoints for the API
+app.MapApiEndpoints();
+app.MapDefaultEndpoints();
+
+app.Run();
