7b31c53e4a9d72adbebb6d065f40b283edf5add5
AG <ag.nayanajith@gmail.com>
2025-04-30 23:26:21 +0530

    Add service discovery

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.Api>("api");
-var app = builder.AddProject<Projects.MyWeatherHub>("myweatherhub");
+var app = builder.AddProject<Projects.MyWeatherHub>("myweatherhub")
+    .WithReference(api)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
